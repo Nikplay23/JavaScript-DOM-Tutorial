bcdeab2e191db4db2d18364cef196a1432990397
Nikplay23 <78353018+Nikplay23@users.noreply.github.com>
2021-03-16 14:39:43 +0200

    Uzdevums

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-var titles = document.getElementsByClassName('title')
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+//console.log(wmf);
 
-Array.from(titles).forEach(function(item){
-    console.log(item);
-});
+var books = document.querySelector('#book-list li .name')
+//console.log(books);
+
+books = document.querySelectorAll('#book-list li .name')
+//console.log(books);
+
+Array.from(books).forEach(function(book){
+    console.log(book)
+})
