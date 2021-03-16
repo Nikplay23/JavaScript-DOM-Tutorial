06741e2786eefb94715240d03403b432e60fcd1e
Nikplay23 <78353018+Nikplay23@users.noreply.github.com>
2021-03-16 15:28:38 +0200

    Uzdevums

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const bookList = document.querySelector('#book-list');
+const list = document.querySelector('#book-list ul')
 
-console.log("book-list next sibling is:",bookList.nextSibling);
-console.log("book-list next element sibling is:",bookList.nextElementSibling);
-
-console.log("book-list previous sibling is:",bookList.previousSibling);
-console.log("book-list previous element sibling is:",bookList.previousElementSibling);
-
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!'
+//delete books
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li)
+    }
+})
